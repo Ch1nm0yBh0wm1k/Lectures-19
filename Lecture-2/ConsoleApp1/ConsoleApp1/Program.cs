1bc55c826f9b11c1a8a3644b1c01c25ae882eb8e
chinmoy <chinmoy.bhowmik.cuet@gmail.com>
2024-09-28 12:30:51 +0600

    Provide Lecture 2

---
+++ Lecture-2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿//// See https://aka.ms/new-console-template for more information
+////using System.Security.Cryptography;
+
+////Console.WriteLine("Hello, World!");
+//using System;
+
+//int b = 2;
+//double a = b;
+//int c = (int)a;
+////int d = Convert.ToInt(a);
+
+////scan input
+////int x = Convert.ToInt32(Console.ReadLine());
+
+//string isTrue = "";
+//String xyz = "";
+
+//int[] arr = { 1, 3, 5};
+
+//foreach(var item in arr)
+//{
+//    Console.WriteLine(item);
+//}
+//PrintSomething();
+
+//static void PrintSomething()
+//{
+//    Console.WriteLine("I am from method");
+//}
+
+//binary search
+
+int[] array = { 1, 2, 3, 4, 8, 0 };
+Array.Sort(array);
+int key = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(BinarySearch(array, key));
+static string BinarySearch(int[] arr, int key)
+{
+    int min = 0;
+    int max = arr.Length - 1;
+    int mid = 0;
+
+    while (min <= max)
+    {
+        mid = (min + max) / 2;
+
+        if (key == arr[mid])
+            return "Found";
+        else if (key < arr[mid])
+            max = mid - 1;
+        else if (key > arr[mid])
+            min = mid + 1;
+
+    }
+    return "Not Found";
+
+
+
+}
+
---
+++ Lecture-2/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,231 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System.Runtime.InteropServices;
+
+//Console.WriteLine("Hello, World!");
+
+//Person person = new Person();
+//person.PrintSomething();
+//class
+//class Person
+//{
+//    string name; //field
+//    string age;
+
+//    public void PrintSomething()//methid
+//    {
+//        Console.WriteLine("Hi");
+//    }
+//}
+
+//constructor
+
+//Person person2 = new Person();
+//Console.WriteLine(person2.name);
+
+//Person person3 = new Person("Rafi", 20);
+//Console.WriteLine(person3.name);
+
+//class Person
+//{
+//    public string name; //field
+//    public int age;
+//    //default constructor
+//    public Person() 
+//    {
+//        name = "abcd";
+//        age = 2;
+//    }
+//    //parameterized constructor
+//    public Person(string Name, int Age)
+//    {
+//        name = Name;
+//        age = Age;
+//    }
+
+//    public void PrintSomething()//methid
+//    {
+//        Console.WriteLine("Hi");
+//    }
+//}
+
+//Access Modifier
+//--public, private, protected, internal
+//public
+//Person person1 = new Person();
+//Console.WriteLine(person1.age);
+////private
+//Console.WriteLine(person1.RetValue());
+//protected
+//Console.WriteLine(person1.name);
+//Car car1 = new Car();
+//Console.WriteLine(car1.ReturnProtected());
+
+//class Person
+//{
+//    public int age= 2;
+//    private string pwd = "jhvcsadjch";
+//    protected string name = "rahul";
+//    public string RetValue()
+//    {
+//        return pwd;
+//    }
+//}
+
+//class Car : Person
+//{
+//    public string ReturnProtected()
+//    {
+//        return name;
+//    }
+//}
+
+
+//static use 
+
+//Person person = new Person();
+//Console.WriteLine(person.DoIncrement());//3
+
+//Person person1 = new Person();
+//Console.WriteLine(person1.age);//2
+
+//Person person3 = new Person();
+//Console.WriteLine(person3.DoIncrementStatic());//11
+
+//Person person4 = new Person();
+//Console.WriteLine(person4.DoIncrementStatic());//12
+
+//class Person
+//{
+//    public int age = 2;
+
+//    public static int age2 = 10;
+
+//    public int DoIncrementStatic()
+//    {
+//        return ++ age2;
+//    }
+
+//    public int DoIncrement()
+//    {
+//        return ++age;
+//    }
+//}
+
+//properties
+
+//Person person = new Person();
+//Console.WriteLine(person.Age); //2
+
+//Person person1 = new Person();
+//person1.Age = 100;
+//Console.WriteLine(person1.Age); //100
+//class Person
+//{
+//    private int age = 2;
+//    //get used for read purpose, set for write
+//    public int Age { get { return age; } set { age = value; } } //property
+//}
+
+//polymorphism
+
+
+//method overloading
+
+//Person person = new Person();
+//person.GetSum(1, 2);
+//person.GetSum(1.0F, 2.0F);
+//person.GetSum(1.0D, 2.0D);
+
+
+//class Person
+//{
+//    public void GetSum(int a, int b)
+//    {
+//        Console.WriteLine("I am Int");
+//    }
+
+//    public void GetSum(float a, float b)
+//    {
+//        Console.WriteLine("I am float");
+//    }
+//    public void GetSum(double a, double b)
+//    {
+//        Console.WriteLine("I am Double");
+//    }
+//}
+
+//method overriding
+
+//Person2 person = new Person2();
+//person.GetSum(1, 2);
+
+
+//class Person
+//{
+//    virtual public void GetSum(int a, int b)
+//    {
+//        Console.WriteLine("I am main");
+//    }
+//}
+
+//class Person2 : Person
+//{
+//    override public void GetSum(int a, int b)
+//    {
+//        Console.WriteLine("I am overwritten");
+//    }
+//}
+
+
+//abstraction
+//Car car = new Car();
+//car.SetValue();
+//abstract class Person
+//{
+//    public int GetValue()
+//    {
+//        return 1;
+//    }
+//    abstract public int SetValue();
+
+//}
+//class Car : Person
+//{
+//    override public int SetValue()
+//    {
+//        return 1;
+//    }
+//}
+
+//interface
+
+interface IPerson
+{
+    public int RetVal();
+}
+interface IPerson2
+{
+    public int RetVal();
+}
+
+class Person5 : IPerson, IPerson2
+{
+    public int RetVal()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Person : IPerson
+{
+    public int RetVal()
+    {
+        return 2;
+    }
+}
+
+class Car : Person
+{
+
+}
+
